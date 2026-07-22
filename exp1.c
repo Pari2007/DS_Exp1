82716a652383bfa0ac656f88e41368f45f6a7c01
Pari Bhatnagar <64839438+Pari2007@users.noreply.github.com>
2026-07-22 12:03:59 +0530

    Refactor exp1.c with two methods for counting elements

--- exp1.c
+++ exp1.c
@@ -1,3 +1,5 @@
+//Method 1
+
 #include<stdio.h>
 int n;
 int i,j,k;
@@ -36,4 +38,53 @@ int main(){
     
     
     return 0;
+}
+
+//Method 2
+
+#include<stdio.h>
+
+int flag;
+int x;
+int n;
+int i;
+int count = 0;
+
+
+int main(){
+	flag = 0;
+	printf("enter the element x");
+	scanf("%d", &x);
+
+	
+	printf("Enter the size of the array");
+    scanf("%d", &n);
+	int arr[n];
+	
+    for(i; i<n; i++){
+        printf("Enter element at pos[%d]",i);
+        scanf("%d",&arr[i]);
+    }
+    
+    for(i=0;i<n;i++){
+    	if(x==arr[i]){
+    		flag = 1;
+    		count++;
+    		if(count == 1){
+				printf("the first occurence of x is at, %d\n", i);
+		}
+    		
+		}
+		
+	}
+
+	if(flag ==0){
+			printf("X not found\n");
+		
+	}
+	
+	printf("the number of occurences of x is %d", count);
+	
+	
+	return 0;
 }
